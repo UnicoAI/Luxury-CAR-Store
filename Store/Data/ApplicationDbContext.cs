7bd98c0140a7e2cd943f52dbb64706d4bab70342
Marius Daniel Boncica <102974083+UnicoAI@users.noreply.github.com>
2024-01-09 02:09:41 +0000

    Add files via upload

--- Store/Data/ApplicationDbContext.cs
+++ Store/Data/ApplicationDbContext.cs
@@ -6,6 +6,7 @@ namespace Store.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
+    public DbSet<Order> Orders { get; set; }
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
@@ -16,6 +17,17 @@ public class ApplicationDbContext : IdentityDbContext
     public DbSet<Category> Categories { get; set; }
     public DbSet<ProductCategory> ProductCategories { get; set; }
     public DbSet<Cart> Carts { get; set; }
+    public class Order
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int ProductId { get; set; }
+        public Product Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -24,6 +36,8 @@ public class ApplicationDbContext : IdentityDbContext
         modelBuilder.Entity<ProductCategory>()
             .HasKey(productCategory => new { productCategory.CategoryId, productCategory.ProductId });
     }
+
+    public DbSet<Store.Models.Order> Order_1 { get; set; } = default!;
 
   
 }
